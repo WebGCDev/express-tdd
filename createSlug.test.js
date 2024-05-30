d13d2cf8610aa10ace500648303c9eeb863aa786
WebGCDev <gaetano.chi@gmail.com>
2024-05-30 16:46:01 +0200

    Add: createSlug incrementa di 1 lo slug quando esiste già

--- createSlug.test.js
+++ createSlug.test.js
@@ -3,16 +3,14 @@ const { expect, it } = require('@jest/globals');
 // importare funzione createSlug dal file createSlug.js
 const createSlug = require('./createSlug');
 
-// verifica che createSlug sostituisca gli spazi con trattini
-test('createSlug dovrebbe sostituire gli spazi con trattini', () => {
-  // Titolo del post da convertire in slug con spazi
+// verifica che createSlug incrementi di 1 lo slug quando esiste già
+test('createSlug dovrebbe incrementare di 1 lo slug quando esiste già', () => {
+  // Titolo del post da convertire in slug
   const titolo = 'Il Milan vince lo scudetto 2025';
-  // Lista di esempio di un solo post con uno slug che corrisponde a quello generato dal titolo
-  const posts = [{ slug: createSlug(titolo, []) }];
+  // Lista di esempio dei post esistenti con uno slug simile già presente
+  const posts = [{ slug: 'il-milan-vince-lo-scudetto-2025' }];
   // Chiamiamo la funzione createSlug con il titolo e la lista di post
   const result = createSlug(titolo, posts);
-  // Verifica: risultato della funzione abbia spazi sostituiti da trattini
-  expect(result).not.toContain(' ');
+  // Risultato della funzione deve avere uno slug incrementato di 1
+  expect(result).toBe('il-milan-vince-lo-scudetto-2025-1');
 });
-
-// Dopo vari test che mi riportavano vari errori ho risolto cosi: ho dovuto passare una lista di post che contiene un solo post con uno slug che corrisponde a quello generato dal titolo.
