d459fb26d7480b4009a6215046f0e49b1829bb4c
WebGCDev <gaetano.chi@gmail.com>
2024-05-30 15:26:56 +0200

    Add: createSlug stringa in lowercase

--- createSlug.test.js
+++ createSlug.test.js
@@ -3,14 +3,14 @@ const { expect, it } = require('@jest/globals');
 // importare funzione createSlug dal file createSlug.js
 const createSlug = require('./createSlug');
 
-//verifica che createSlug ritorni una stringa con JEST
-test('createSlug dovrebbe ritornare una stringa', () => {
-  //titolo del post da convertire in slug
-  const titolo = 'Il Milan vince lo scudetto 2025';
-  //lista di esempio dei post esistenti con i loro slug
+// verifica che createSlug ritorni una stringa in lowercase
+test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
+  // Titolo del post da convertire in slug
+  const titolo = 'Il Milan Vince Lo Scudetto 2025';
+  // Lista di esempio dei post esistenti con i loro slug
   const posts = [{ slug: 'il-milan-vince-lo-scudetto-2025' }];
-  //Chiamiamo la funzione createSlug con il titolo e la lista di post
+  // Chiamiamo la funzione createSlug con il titolo e la lista di post
   const result = createSlug(titolo, posts);
-  // Verifica: risultato della funzione sia di tipo stringa
-  expect(typeof result).toBe('string');
+  // Verifica: risultato della funzione sia in lowercase
+  expect(result).toBe(result.toLowerCase());
 });
